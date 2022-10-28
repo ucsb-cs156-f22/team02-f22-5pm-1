db43159b2cc24b383f1a343b90a4dd3ac01a80a1
Ethan Lee <elee1819@gmail.com>
2022-10-27 17:00:05 -0700

    Finished edit endpoint and its tests  for MenuItemReview

--- src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/MenuItemReviewController.java
@@ -94,4 +94,25 @@ public class MenuItemReviewController extends ApiController {
         menuItemReviewRepository.delete(menuItemReview);
         return genericMessage("MenuItemReview with id %s deleted".formatted(id));
     }
+
+    @ApiOperation(value = "Update a single review")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public MenuItemReview updateMenuItemReview(
+            @ApiParam("id") @RequestParam Long id,
+            @RequestBody @Valid MenuItemReview incoming) {
+
+        MenuItemReview menuItemReview = menuItemReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        menuItemReview.setItemId(incoming.getItemId());
+        menuItemReview.setReviewerEmail(incoming.getReviewerEmail());
+        menuItemReview.setStars(incoming.getStars());
+        menuItemReview.setDateReviewed(incoming.getDateReviewed());
+        menuItemReview.setComments(incoming.getComments());
+
+        menuItemReviewRepository.save(menuItemReview);
+
+        return menuItemReview;
+    }
 }
