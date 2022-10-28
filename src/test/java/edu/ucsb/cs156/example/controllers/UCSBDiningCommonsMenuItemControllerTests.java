2885fe25d5211e8c011895b6ea06008018d110e2
Gianmarco Psotka <gianmarco@umail.ucsb.edu>
2022-10-27 17:46:10 -0700

    Fixed tests for Get endpoint for single database row

--- src/test/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemControllerTests.java
@@ -74,6 +74,89 @@ public class UCSBDiningCommonsMenuItemControllerTests extends ControllerTestCase
                                 .andExpect(status().is(403)); // only admins can post
         }
 
+        // // Tests with mocks for database actions
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
+
+                
+
+                UCSBDiningCommonsMenu ucsbDiningCommonsMenu = UCSBDiningCommonsMenu.builder()
+                                .name("Chicken Caeser Salad")
+                                .diningCommonsCode("ortega")
+                                .station("Entrees")
+                                .build();
+
+                when(ucsbDiningCommonsMenuRepository.findById(eq(7L))).thenReturn(Optional.of(ucsbDiningCommonsMenu));
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbdiningcommonsmenu?id=7"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(ucsbDiningCommonsMenuRepository, times(1)).findById(eq(7L));
+                String expectedJson = mapper.writeValueAsString(ucsbDiningCommonsMenu);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
+
+                // arrange
+
+                when(ucsbDiningCommonsMenuRepository.findById(eq(7L))).thenReturn(Optional.empty());
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbdiningcommonsmenu?id=7"))
+                                .andExpect(status().isNotFound()).andReturn();
+
+                // assert
+
+                verify(ucsbDiningCommonsMenuRepository, times(1)).findById(eq(7L));
+                Map<String, Object> json = responseToJson(response);
+                assertEquals("EntityNotFoundException", json.get("type"));
+                assertEquals("UCSBDiningCommonsMenu with id 7 not found", json.get("message"));
+        }
+
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_ucsbdates() throws Exception {
+
+
+                UCSBDiningCommonsMenu ucsbDiningCommonsMenu1 = UCSBDiningCommonsMenu.builder()
+                                .name("Chicken Caesar Salad")
+                                .diningCommonsCode("ortega")
+                                .station("Entrees")
+                                .build();
+
+
+                UCSBDiningCommonsMenu ucsbDiningCommonsMenu2 = UCSBDiningCommonsMenu.builder()
+                                .name("Baked Pesto Pasta with Chicken")
+                                .diningCommonsCode("ortega")
+                                .station("Entree Specials")
+                                .build();
+
+                ArrayList<UCSBDiningCommonsMenu> expectedDiningCommonsMenu = new ArrayList<>();
+                expectedDiningCommonsMenu.addAll(Arrays.asList(ucsbDiningCommonsMenu1, ucsbDiningCommonsMenu2));
+
+                when(ucsbDiningCommonsMenuRepository.findAll()).thenReturn(expectedDiningCommonsMenu);
+
+                // act
+                MvcResult response = mockMvc.perform(get("/api/ucsbdiningcommonsmenu/all"))
+                                .andExpect(status().isOk()).andReturn();
+
+                // assert
+
+                verify(ucsbDiningCommonsMenuRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedDiningCommonsMenu);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
+
         
 }
 
