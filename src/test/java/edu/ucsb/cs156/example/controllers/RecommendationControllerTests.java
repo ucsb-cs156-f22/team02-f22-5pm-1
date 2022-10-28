9e1e513346277c8238472a33f540e725390e0a84
isaac <ihoffman@ucsb.edu>
2022-10-27 17:18:30 -0700

    ih - remove extraneous code (get by id)

--- src/test/java/edu/ucsb/cs156/example/controllers/RecommendationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/RecommendationControllerTests.java
@@ -42,12 +42,6 @@ public class RecommendationControllerTests extends ControllerTestCase {
 
     // Authorization tests for get
 
-    @Test
-    public void logged_out_users_cannot_get_all() throws Exception {
-        mockMvc.perform(get("/api/recommendations/all"))
-                .andExpect(status().is(403)); // logged out users can't get all
-    }
-
     @WithMockUser(roles = { "USER" })
     @Test
     public void logged_in_users_can_get_all() throws Exception {
@@ -105,55 +99,6 @@ public class RecommendationControllerTests extends ControllerTestCase {
 
         verify(recommendationRepository, times(1)).findAll();
         String expectedJson = mapper.writeValueAsString(expectedRecs);
-        String responseString = response.getResponse().getContentAsString();
-        assertEquals(expectedJson, responseString);
-    }
-
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception {
-
-        // arrange
-
-        when(recommendationRepository.findById(eq(7L))).thenReturn(Optional.empty());
-
-        // act
-
-        MvcResult response = mockMvc.perform(get("/api/recommendations?id=7"))
-                        .andExpect(status().isNotFound()).andReturn();
-        
-        // assert
-
-        verify(recommendationRepository, times(1)).findById(eq(7L));
-        
-    }
-
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void test_that_logged_in_user_can_get_by_id_when_the_id_exists() throws Exception {
-
-        // arrange
-        LocalDateTime ldt = LocalDateTime.parse("2022-01-03T00:00:00");
-
-        Recommendation recommendation = Recommendation.builder()
-                        .requesterEmail("requester@mail.com")
-                        .professorEmail("professor@mail.com")
-                        .explanation("test")
-                        .dateRequested(ldt)
-                        .dateNeeded(ldt)
-                        .done(false)
-                        .build();
-
-        when(recommendationRepository.findById(eq(7L))).thenReturn(Optional.of(recommendation));
-
-        // act
-        MvcResult response = mockMvc.perform(get("/api/recommendations?id=7"))
-                        .andExpect(status().isOk()).andReturn();
-
-        // assert
-
-        verify(recommendationRepository, times(1)).findById(eq(7L));
-        String expectedJson = mapper.writeValueAsString(recommendation);
         String responseString = response.getResponse().getContentAsString();
         assertEquals(expectedJson, responseString);
     }
