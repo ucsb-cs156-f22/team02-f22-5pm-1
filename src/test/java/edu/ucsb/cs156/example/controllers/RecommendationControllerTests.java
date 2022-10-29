5ab73a53879be658af11ab0062dcc72fa1b2a677
isaac <ihoffman@ucsb.edu>
2022-10-28 22:52:33 -0700

    ih - fix delete tests for recommendation

--- src/test/java/edu/ucsb/cs156/example/controllers/RecommendationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/RecommendationControllerTests.java
@@ -213,10 +213,9 @@ public class RecommendationControllerTests extends ControllerTestCase {
                     .build();
 
         when(recommendationRepository.findById(eq(15L))).thenReturn(Optional.of(recommendation));
-
         // act
         MvcResult response = mockMvc.perform(
-                        delete("/api/ucsbdates?id=15")
+                        delete("/api/recommendations?id=15")
                                         .with(csrf()))
                         .andExpect(status().isOk()).andReturn();
 
@@ -226,5 +225,25 @@ public class RecommendationControllerTests extends ControllerTestCase {
 
         Map<String, Object> json = responseToJson(response);
         assertEquals("Recommendation with id 15 deleted", json.get("message"));
+    }
+
+    @WithMockUser(roles = { "ADMIN", "USER" })
+    @Test
+    public void admin_tries_to_delete_non_existant_recommendation_and_gets_right_error_message()
+                throws Exception {
+        // arrange
+
+        when(recommendationRepository.findById(eq(15L))).thenReturn(Optional.empty());
+
+        // act
+        MvcResult response = mockMvc.perform(
+                        delete("/api/recommendations?id=15")
+                                        .with(csrf()))
+                        .andExpect(status().isNotFound()).andReturn();
+
+        // assert
+        verify(recommendationRepository, times(1)).findById(15L);
+        Map<String, Object> json = responseToJson(response);
+        assertEquals("Recommendation with id 15 not found", json.get("message"));
     }
 }
